9a78f43b83e930668817dbe9f8f4e7189855785b
linghengqian <linghengqian@outlook.com>
2024-09-25 12:10:28 +0800

    The unit tests execute fine under shardingsphere 5.5.0

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # ShardingSphere-Test
 
-> This project reproduce 'connection disabled' Exception when use both ShardingSphere and Druid
-> 
-> src/test/java/com/hulalaga/ShardingSphereTest.java
-> 
+- For https://github.com/apache/shardingsphere/issues/32765 .
+
+- Verified unit test under `Ubuntu 22.04.4 LTS` with `Docker Engine` and `SDKMAN!`.
 
 ```shell
-mvn test
+sdk install java 8.0.422-tem
+git clone git@github.com:linghengqian/shardingsphere-test.git
+cd ./shardingsphere-test/
+sdk use java 8.0.422-tem
+./mvnw -T 1C clean test 
+```
+
+- The unit tests execute fine.
+
+```shell
+[INFO] Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 14.55 s -- in com.hulalaga.ShardingSphereTest
+[INFO] 
+[INFO] Results:
+[INFO] 
+[INFO] Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+[INFO] 
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  16.956 s
+[INFO] Finished at: 2024-09-25T12:06:17+08:00
+[INFO] ------------------------------------------------------------------------
 ```
--- src/test/java/com/hulalaga/ShardingSphereTest.java
+++ src/test/java/com/hulalaga/ShardingSphereTest.java
@@ -1,108 +1,65 @@
 package com.hulalaga;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.shardingsphere.driver.ShardingSphereDriver;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.sql.init.DatabaseInitializationMode;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.com.google.common.collect.ImmutableList;
+import org.testcontainers.utility.DockerImageName;
 
-import java.io.BufferedWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.stream.IntStream;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SuppressWarnings({"SqlNoDataSourceInspection", "resource"})
 @Testcontainers
 public class ShardingSphereTest {
-    private static final ResourcePatternResolver RESOURCE_PATTERN_RESOLVER = new PathMatchingResourcePatternResolver();
-    @Container
-    private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>()
-            .withUrlParam("userSSL", "false")
-            .withDatabaseName("the_database");
     private static JdbcTemplate JDBC_TEMPLATE;
 
+    @Container
+    public static final MySQLContainer<?> CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:5.7.34"))
+            .withDatabaseName("the_database")
+            .withInitScript("init-schema.sql");
+
     @BeforeAll
-    public static void dataSource() throws Exception {
-        final DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUsername(MY_SQL_CONTAINER.getUsername());
-        dataSource.setPassword(MY_SQL_CONTAINER.getPassword());
-        dataSource.setUrl(MY_SQL_CONTAINER.getJdbcUrl());
-
-        // ### init-schema
-        final SqlInitializationProperties sqlInitializationProperties = new SqlInitializationProperties();
-        sqlInitializationProperties.setMode(DatabaseInitializationMode.ALWAYS);
-        sqlInitializationProperties.setSchemaLocations(
-                ImmutableList.of(
-                        "classpath:init-schema.sql"
-                )
-        );
-        final SqlDataSourceScriptDatabaseInitializer sqlDataSourceScriptDatabaseInitializer = new SqlDataSourceScriptDatabaseInitializer(
-                DataSourceBuilder.derivedFrom(dataSource)
-                        .username("root")
-                        .build(),
-                SqlDataSourceScriptDatabaseInitializer.getSettings(
-                        sqlInitializationProperties
-                )
-        );
-        sqlDataSourceScriptDatabaseInitializer.setResourceLoader(
-                RESOURCE_PATTERN_RESOLVER
-        );
-        sqlDataSourceScriptDatabaseInitializer.afterPropertiesSet();
-
-        // ### build jdbcTemplate
-
-        // this datasource can't cause 'connection disabled', because no connection pool
-//        final DataSource ds = ShardingSphereDataSourceFactory.createDataSource(
-//                MY_SQL_CONTAINER.getDatabaseName(),
-//                ImmutableMap.of(
-//                        "db", dataSource
-//                ),
-//                Collections.emptyList(),
-//                new Properties()
-//        );
-        final Resource resource = RESOURCE_PATTERN_RESOLVER.getResource("classpath:shardingsphere.yaml");
-        String shardingsphereConfig =
-                String.join("\n", Files.readAllLines(Paths.get(resource.getURI())));
-        shardingsphereConfig = shardingsphereConfig.replace("<MYSQL_DATABASE>", MY_SQL_CONTAINER.getDatabaseName());
-        shardingsphereConfig = shardingsphereConfig.replace("<MYSQL_JDBC_URL>", MY_SQL_CONTAINER.getJdbcUrl());
-        shardingsphereConfig = shardingsphereConfig.replace("<MYSQL_USER>", MY_SQL_CONTAINER.getUsername());
-        shardingsphereConfig = shardingsphereConfig.replace("<MYSQL_PASSWORD>", MY_SQL_CONTAINER.getPassword());
-        final Path tempFile = Files.createTempFile(null, "shardingsphere.yaml");
-        try (BufferedWriter writer = Files.newBufferedWriter(tempFile)) {
-            writer.write(shardingsphereConfig);
-        }
-        // this pool datasource will cause 'connection disabled',
-        // because 'getConnections' reuse connections which disabled by DruidPooledConnection
-        // in org.apache.shardingsphere.driver.jdbc.core.connection.ConnectionManager#getConnections
+    static void initDataSource() {
+        assertThat(System.getProperty("fixture.mysql.db.url"), is(nullValue()));
+        assertThat(System.getProperty("fixture.mysql.db.username"), is(nullValue()));
+        assertThat(System.getProperty("fixture.mysql.db.password"), is(nullValue()));
+        assertThat(System.getProperty("fixture.mysql.db.databaseName"), is(nullValue()));
+        System.setProperty("fixture.mysql.db.url", CONTAINER.getJdbcUrl());
+        System.setProperty("fixture.mysql.db.username", CONTAINER.getUsername());
+        System.setProperty("fixture.mysql.db.password", CONTAINER.getPassword());
+        System.setProperty("fixture.mysql.db.databaseName", CONTAINER.getDatabaseName());
         final HikariDataSource poolDataSource = DataSourceBuilder.create()
                 .type(HikariDataSource.class)
                 .driverClassName(ShardingSphereDriver.class.getName())
-                .url("jdbc:shardingsphere:" + tempFile)
+                .url("jdbc:shardingsphere:classpath:shardingsphere.yaml?placeholder-type=system_props")
                 .build();
         JDBC_TEMPLATE = new JdbcTemplate(poolDataSource);
     }
 
+    @AfterAll
+    static void afterAll() {
+        System.clearProperty("fixture.mysql.db.url");
+        System.clearProperty("fixture.mysql.db.username");
+        System.clearProperty("fixture.mysql.db.password");
+        System.clearProperty("fixture.mysql.db.databaseName");
+    }
+
     @Test
     public void testDruidConnectionDisabled() {
-        for (int i = 0; i < 50; i++) {
-            Assertions.assertDoesNotThrow(() -> {
-                JDBC_TEMPLATE.execute("select ''");
-            });
-            Assertions.assertThrows(Exception.class, () -> {
-                JDBC_TEMPLATE.execute("insert into the_table(id) values(1)");
-            });
-        }
+        IntStream.range(0, 50).forEachOrdered(i -> {
+            assertDoesNotThrow(() -> JDBC_TEMPLATE.execute("select ''"));
+            assertDoesNotThrow(() -> JDBC_TEMPLATE.execute("insert into the_table(id) values(1)"));
+        });
     }
 }
